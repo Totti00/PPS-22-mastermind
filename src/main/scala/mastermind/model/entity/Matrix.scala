5e8fc624567c725f018f1f5595224ebcc9ecb797
Ramzi Gallala <ramzi.gallala@studio.unibo.it>
2025-03-03 23:05:58 +0100

    refactor code

--- src/main/scala/mastermind/model/entity/Matrix.scala
+++ src/main/scala/mastermind/model/entity/Matrix.scala
@@ -3,7 +3,7 @@ package mastermind.model.entity
 trait Matrix[T]:
   def elements: Vector[Vector[T]]
   def rows: Int = elements.size
-  def cols: Int = if (elements.nonEmpty) elements.head.size else 0
+  def cols: Int = if elements.nonEmpty then elements.head.size else 0
   def cell(row: Int, col: Int): T = elements(row)(col)
   def row(row: Int): Seq[T] = elements(row)
   def replaceCell(row: Int, col: Int, cell: T): Matrix[T]
@@ -17,4 +17,6 @@ object Matrix:
   private case class MatrixImpl[T](override val elements: Vector[Vector[T]]) extends Matrix[T]:
     assert(elements != null)
     override def replaceRow(row: Int, vec: Vector[T]): Matrix[T] = copy(elements.updated(row, vec))
-    override def replaceCell(row: Int, col: Int, cell: T): Matrix[T] = copy(elements.updated(row, elements(row).updated(col, cell)))
+    override def replaceCell(row: Int, col: Int, cell: T): Matrix[T] = copy(
+      elements.updated(row, elements(row).updated(col, cell))
+    )
--- src/main/scala/mastermind/model/entity/Stone.scala
+++ src/main/scala/mastermind/model/entity/Stone.scala
@@ -5,37 +5,44 @@ sealed trait Stone:
 
 sealed trait PlayableStone extends Stone
 
-case object Red extends PlayableStone{override def stringRepresentation: String = "Red"}
-case object Green extends PlayableStone{override def stringRepresentation: String = "Green"}
-case object Blue extends PlayableStone{override def stringRepresentation: String = "Blue"}
-case object Yellow extends PlayableStone{override def stringRepresentation: String = "Yellow"}
-case object White extends PlayableStone{override def stringRepresentation: String = "White"}
-case object Purple extends PlayableStone{override def stringRepresentation: String = "Purple"}
-case object Empty extends PlayableStone{override def stringRepresentation: String = "Empty"}
+case object Red extends PlayableStone:
+  override def stringRepresentation: String = "Red"
+case object Green extends PlayableStone:
+  override def stringRepresentation: String = "Green"
+case object Blue extends PlayableStone:
+  override def stringRepresentation: String = "Blue"
+case object Yellow extends PlayableStone:
+  override def stringRepresentation: String = "Yellow"
+case object White extends PlayableStone:
+  override def stringRepresentation: String = "White"
+case object Purple extends PlayableStone:
+  override def stringRepresentation: String = "Purple"
+case object Empty extends PlayableStone:
+  override def stringRepresentation: String = "Empty"
 
 object PlayableStone:
   def apply(stringRepresentation: String): Stone = stringRepresentation match
-    case "Red" => Red
-    case "Green" => Green
-    case "Blue" => Blue
+    case "Red"    => Red
+    case "Green"  => Green
+    case "Blue"   => Blue
     case "Yellow" => Yellow
-    case "White" => White
+    case "White"  => White
     case "Purple" => Purple
-    case "Empty" => Empty
-    case _ => Empty // Default case
-
+    case "Empty"  => Empty
+    case _        => Empty // Default case
 
 sealed trait HintStone extends Stone
 
-case object HintRed extends HintStone {override def stringRepresentation = "Red" }
-case object HintWhite extends HintStone {override def stringRepresentation = "White" }
-case object HintEmpty extends HintStone {override def stringRepresentation = "Empty" }
-
+case object HintRed extends HintStone:
+  override def stringRepresentation = "Red"
+case object HintWhite extends HintStone:
+  override def stringRepresentation = "White"
+case object HintEmpty extends HintStone:
+  override def stringRepresentation = "Empty"
 
 object HintStone:
-  def apply(stringRepresentation: String): HintStone = stringRepresentation match 
+  def apply(stringRepresentation: String): HintStone = stringRepresentation match
     case "R" => HintRed
     case "W" => HintWhite
     case "E" => HintEmpty
-    case _   => HintEmpty  // Default case
-  
+    case _   => HintEmpty // Default case
