b7b4eda7bd02f8c3060593be065d974c9c6968be
Totti00 <gek5800@gmail.com>
2025-03-03 14:03:34 +0100

    formatted

--- src/main/scala/mastermind/model/entity/Code.scala
+++ src/main/scala/mastermind/model/entity/Code.scala
@@ -5,6 +5,6 @@ trait Code:
 
 object Code:
   def apply(size: Int): Code = CodeImpl(Vector.fill(size)(5))
-  
+
   private case class CodeImpl(code: Vector[Int]) extends Code:
     override def size: Int = code.size
