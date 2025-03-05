0ebd321c46b57879ba3cc028d56970084c0d9312
Ramzi Gallala <ramzi.gallala@studio.unibo.it>
2025-03-05 09:27:00 +0100

    refactored code

--- src/main/scala/mastermind/model/entity/Code.scala
+++ src/main/scala/mastermind/model/entity/Code.scala
@@ -6,34 +6,15 @@ trait Code:
 object Code:
   private type SecretCode = Vector[PlayableStone]
   private type PlayableCode = Vector[PlayableStone]
-  private type HintCode = Vector[HintStone]
 
   def apply(size: Int): Code = CodeImpl(Vector.fill(size)(PlayableStone.random))
 
   private case class CodeImpl(code: SecretCode) extends Code:
     override def compareTo(userInput: PlayableCode): Vector[HintStone] =
-      val equalsList = compareToEqual(userInput)
-      val presentList = compareToPresent(userInput, equalsList)
-      buildVector(equalsList.size, presentList.size)
+      println(code)
+      println(userInput)
+      val testHintRow = Vector(HintStone("Red"), HintStone("White"), HintStone("White"), HintStone("Empty"))
+      testHintRow
 
-    // Identifica le posizioni uguali tra il codice e l'input dell'utente
-    private def compareToEqual(inputUser: PlayableCode): List[Int] =
-      code
-        .zip(inputUser)
-        .zipWithIndex
-        .collect {
-          case ((c, u), idx) if c == u => idx
-        }
-        .toList
 
-    // Identifica le posizioni presenti ma non uguali (senza duplicati)
-    private def compareToPresent(inputUser: PlayableCode, equalsList: List[Int]): List[Int] =
-      (for
-        (c, idx) <- code.zipWithIndex
-        if !equalsList.contains(idx) && inputUser.exists(u => u == c && !equalsList.contains(inputUser.indexOf(u)))
-      yield idx).distinct.toList
-
-    private def buildVector(equalCount: Int, presentCount: Int): HintCode =
-      Vector.fill(equalCount)(HintStone("Red")) ++
-        Vector.fill(presentCount)(HintStone("White")) ++
-        Vector.fill(code.size - equalCount - presentCount)(HintStone("Empty"))
+    
