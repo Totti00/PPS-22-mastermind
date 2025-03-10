ca4b1661e81034f815d01dc6eb32c83db32c02d4
Ramzi Gallala <ramzi.gallala@studio.unibo.it>
2025-03-10 13:51:59 +0100

    implemented class code to check the validity of the user guess

--- src/main/scala/mastermind/model/entity/Code.scala
+++ src/main/scala/mastermind/model/entity/Code.scala
@@ -1,17 +1,44 @@
 package mastermind.model.entity
+
+import mastermind.model.entity.Code.{HintStones, PlayableStones}
+
 //TODO capire se posso vedere code come una variabile e fare override del compareTo associato. Sono quasi sicuro si possa fare e che sia più elegante come soluzione
 trait Code:
-  def compareTo(userInput: Vector[PlayerStoneGrid]): Vector[HintStone]
+  def compareTo(userInput: PlayableStones): HintStones
 
 object Code:
-  private type SecretCode = Vector[PlayerStoneGrid]
-  private type PlayableCode = Vector[PlayerStoneGrid]
+
+  private type PlayableStones = Vector[PlayerStoneGrid]
+  private type HintStones = Vector[HintStone]
 
   def apply(size: Int): Code = CodeImpl(Vector.fill(size)(PlayerStoneGrid.random))
+  def apply(inputCode: PlayableStones): Code = CodeImpl(inputCode)
 
-  private case class CodeImpl(code: SecretCode) extends Code:
-    override def compareTo(userInput: PlayableCode): Vector[HintStone] =
-      println(code)
-      println(userInput)
-      val testHintRow = Vector(HintStone("Red"), HintStone("White"), HintStone("White"), HintStone("Empty"))
-      testHintRow
+  private case class CodeImpl(code: PlayableStones) extends Code:
+    override def compareTo(userInput: PlayableStones): HintStones =
+      val firstRuleVector = compareToEqual(userInput)
+      val secondRuleVector = compareToPresent(userInput)
+      val rulesJoinedVector = joinVectors(firstRuleVector, secondRuleVector)
+      rulesJoinedVector
+
+    private def compareToEqual(inputUser: PlayableStones): HintStones =
+      code.zip(inputUser).zipWithIndex.foldLeft(Vector.empty: Vector[HintStone]) {
+        case (res, ((stoneCode, stoneUser), index))
+            if stoneCode.stringRepresentation.equals(stoneUser.stringRepresentation) =>
+          res :+ HintStone("White")
+        case (res, ((stoneCode, stoneUser), index)) => res :+ HintStone("Empty")
+      }
+
+    private def compareToPresent(inputUser: PlayableStones): HintStones =
+      inputUser.zipWithIndex.foldLeft(Vector.empty: HintStones) {
+        case (res, (stoneUser, index))
+            if code.contains(stoneUser) && !code(index).stringRepresentation.equals(stoneUser.stringRepresentation) =>
+          res :+ HintStone("Red")
+        case (res, (stoneUser, index)) => res :+ HintStone("Empty")
+      }
+
+    private def joinVectors(hintStonesVector: HintStones, hintStonesVector2: HintStones): HintStones =
+      hintStonesVector.zip(hintStonesVector2).foldLeft(Vector.empty: HintStones) {
+        case (res, (hintStone, hintStone2)) if hintStone.stringRepresentation.equals("Empty") => res :+ hintStone2
+        case (res, (hintStone, hintStone2))                                                   => res :+ hintStone
+      }
--- src/test/scala/mastermind/model/entity/CodeTest.scala
+++ src/test/scala/mastermind/model/entity/CodeTest.scala
@@ -1,3 +1,43 @@
 package mastermind.model.entity
 
-class CodeTest
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CodeTest extends AnyFlatSpec:
+
+  /*
+//test to check the validity of the rules
+  "Code" should "find the stones in the right position" in {
+    val inputCode = Vector(PlayerStoneGrid("Yellow"), PlayerStoneGrid("White"), PlayerStoneGrid("White"), PlayerStoneGrid("Yellow"))
+    val userInput =
+      Vector(PlayerStoneGrid("Red"), PlayerStoneGrid("Blue"), PlayerStoneGrid("Green"), PlayerStoneGrid("Yellow"))
+    val hintResult = Vector(HintStone("Empty"), HintStone("Empty"), HintStone("Empty"), HintStone("White"))
+    val code = Code(inputCode)
+
+    assert(hintResult == code.compareTo(userInput))
+
+
+  }
+
+  "Code" should "find the stones in the wrong position" in {
+    val inputCode = Vector(PlayerStoneGrid("Green"), PlayerStoneGrid("White"), PlayerStoneGrid("White"), PlayerStoneGrid("Yellow"))
+    val userInput =
+      Vector(PlayerStoneGrid("Red"), PlayerStoneGrid("Blue"), PlayerStoneGrid("Green"), PlayerStoneGrid("Yellow"))
+    val hintResult = Vector(HintStone("Empty"), HintStone("Empty"), HintStone("Red"), HintStone("Empty"))
+    val code = Code(inputCode)
+
+    assert(hintResult == code.compareTo(userInput))
+
+
+  }
+   */
+  "Code" should "check the user input to find the hintVector" in {
+    val inputCode =
+      Vector(PlayerStoneGrid("Green"), PlayerStoneGrid("White"), PlayerStoneGrid("White"), PlayerStoneGrid("Yellow"))
+    val userInput =
+      Vector(PlayerStoneGrid("Red"), PlayerStoneGrid("Blue"), PlayerStoneGrid("Green"), PlayerStoneGrid("Yellow"))
+    val hintResult = Vector(HintStone("Empty"), HintStone("Empty"), HintStone("Red"), HintStone("White"))
+    val code = Code(inputCode)
+
+    assert(hintResult == code.compareTo(userInput))
+
+  }
