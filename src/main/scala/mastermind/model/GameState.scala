c97b5614dbdc1951d235604a8f5ba25805f1c45e
Totti00 <gek5800@gmail.com>
2025-03-10 15:49:48 +0100

    formatted

--- src/main/scala/mastermind/model/GameState.scala
+++ src/main/scala/mastermind/model/GameState.scala
@@ -1,28 +1,22 @@
 package mastermind.model
 
-/**
- * Represents the current state of the game.
- */
+/** Represents the current state of the game.
+  */
 sealed trait GameState
 
 object GameState:
-  /**
-   * The game is in progress.
-   */
+  /** The game is in progress.
+    */
   case object InGame extends GameState
 
-  /**
-   * The player has won the game.
-   */
+  /** The player has won the game.
+    */
   case object PlayerWin extends GameState
 
-  /**
-   * The player has lost the game.
-   */
+  /** The player has lost the game.
+    */
   case object PlayerLose extends GameState
 
-  /**
-   * The player has quit the game.
-   */
+  /** The player has quit the game.
+    */
   case object MainMenu extends GameState
-
