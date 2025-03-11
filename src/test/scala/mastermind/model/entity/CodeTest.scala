678c5bb7a1d58c10aa4493f9b8f4442f6f57d058
Ramzi Gallala <ramzi.gallala@studio.unibo.it>
2025-03-11 19:22:25 +0100

    added new test for code

--- src/test/scala/mastermind/model/entity/CodeTest.scala
+++ src/test/scala/mastermind/model/entity/CodeTest.scala
@@ -33,11 +33,29 @@ class CodeTest extends AnyFlatSpec:
   "Code" should "check the user input to find the hintVector" in {
     val inputCode =
       Vector(PlayerStoneGrid("Green"), PlayerStoneGrid("White"), PlayerStoneGrid("White"), PlayerStoneGrid("Yellow"))
-    val userInput =
-      Vector(PlayerStoneGrid("Red"), PlayerStoneGrid("Blue"), PlayerStoneGrid("Green"), PlayerStoneGrid("Yellow"))
-    val hintResult = Vector(HintStone("Empty"), HintStone("Empty"), HintStone("Red"), HintStone("White"))
+    val userInput1 =
+      Vector(PlayerStoneGrid("White"), PlayerStoneGrid("White"), PlayerStoneGrid("White"), PlayerStoneGrid("White"))
+    val userInput2 =
+      Vector(PlayerStoneGrid("Red"), PlayerStoneGrid("Red"), PlayerStoneGrid("Red"), PlayerStoneGrid("Red"))
+    val userInput3 =
+      Vector(PlayerStoneGrid("Purple"), PlayerStoneGrid("Purple"), PlayerStoneGrid("Purple"), PlayerStoneGrid("Purple"))
+    val userInput4 =
+      Vector(PlayerStoneGrid("Yellow"), PlayerStoneGrid("Yellow"), PlayerStoneGrid("Yellow"), PlayerStoneGrid("Yellow"))
+    val userInput5 =
+      Vector(PlayerStoneGrid("Blue"), PlayerStoneGrid("Blue"), PlayerStoneGrid("Blue"), PlayerStoneGrid("Blue"))
+    val userInput6 =
+      Vector(PlayerStoneGrid("Green"), PlayerStoneGrid("Green"), PlayerStoneGrid("Green"), PlayerStoneGrid("Green"))
+
+    val hintResult = Vector(HintStone("Red"), HintStone("White"), HintStone("White"), HintStone("Red"))
     val code = Code(inputCode)
 
-    assert(hintResult == code.compareTo(userInput))
+    assert(hintResult == code.compareTo(userInput1))
+    assert(hintResult != code.compareTo(userInput2))
+    assert(hintResult != code.compareTo(userInput3))
+    assert(hintResult != code.compareTo(userInput4))
+    assert(hintResult != code.compareTo(userInput5))
+    assert(hintResult != code.compareTo(userInput6))
+
+    // assert(hintResult == code.compareTo(userInput))
 
   }
