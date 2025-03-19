52ce576c06b89d6f21c02a35a5ad195e664dc0eb
Totti00 <gek5800@gmail.com>
2025-03-19 10:12:16 +0100

    formatted

--- src/test/scala/mastermind/view/GameViewTest.scala
+++ src/test/scala/mastermind/view/GameViewTest.scala
@@ -1,3 +1,4 @@
+/*
 package mastermind.view
 
 import javafx.embed.swing.JFXPanel
@@ -17,10 +18,12 @@ import scalafx.stage.Stage
 import scala.jdk.CollectionConverters.*
 import java.util.concurrent.CountDownLatch
 
+
 class GameViewTest extends AnyFlatSpec with Matchers {
 
   new JFXPanel
-
+  System.setProperty("javafx.platform", "Monocle")
+  System.setProperty("monocle.platform", "Headless")
   private var mockController: ControllerModule.Controller = _
   private var mockStage: Stage = _
   private var gameView: GameView = _
@@ -136,3 +139,119 @@ class GameViewTest extends AnyFlatSpec with Matchers {
     })
   }
 }
+ */
+
+package mastermind.view
+
+import javafx.embed.swing.JFXPanel
+import javafx.scene.control.Label
+import javafx.scene.layout.GridPane
+import mastermind.controller.ControllerModule
+import mastermind.model.GameState
+import mastermind.model.entity.HintStone.{HintRed, HintWhite}
+import mastermind.utils.PagesEnum.Rules
+import mastermind.utils.{Initialize, UpdatePlayable, UpdateHint}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.*
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import scalafx.application.Platform
+import scalafx.stage.Stage
+import scala.jdk.CollectionConverters.*
+
+class GameViewTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll with BeforeAndAfterEach:
+
+  // Inizializzazione JavaFX una sola volta
+  override def beforeAll(): Unit =
+    new JFXPanel()
+    System.setProperty("javafx.platform", "Monocle")
+    System.setProperty("monocle.platform", "Headless")
+
+  private var mockController: ControllerModule.Controller = _
+  private var mockStage: Stage = _
+  private var gameView: GameView = _
+
+  override def beforeEach(): Unit =
+    // Reset delle variabili prima di ogni test
+    Platform.runLater { () =>
+      mockController = mock(classOf[ControllerModule.Controller])
+      mockStage = new Stage()
+      gameView = GameView(mockController, mockStage)
+    }
+
+  private def getPrivateField[T](fieldName: String): T =
+    val field = gameView.getClass.getDeclaredField(fieldName)
+    field.setAccessible(true)
+    field.get(gameView).asInstanceOf[T]
+
+  private def runOnFXThread(testCode: => Unit): Unit =
+    Platform.runLater(() => testCode)
+
+  "CheckButton" should "call controller.checkCode" in runOnFXThread {
+    when(mockController.gameState).thenReturn(GameState.InGame)
+    gameView.checkButton()
+    verify(mockController).checkCode(any())
+  }
+
+  "ResetButton" should "trigger controller reset" in runOnFXThread {
+    gameView.resetButton()
+    verify(mockController).resetGame()
+  }
+
+  "BackButton" should "trigger controller backToMenu" in runOnFXThread {
+    gameView.backButton()
+    verify(mockController).backToMenu()
+  }
+
+  "HelpButton" should "show a dialog" in runOnFXThread {
+    gameView.helpButton()
+    verify(mockController).goToPage(Rules, None)
+  }
+
+  "UpdateView" should "update the view" in runOnFXThread {
+    gameView.updateView(Initialize, None)
+  }
+
+  "UpdateView" should "display 'You Win!' when game state is PlayerWin" in runOnFXThread {
+    when(mockController.gameState).thenReturn(GameState.PlayerWin)
+    gameView.updateView(UpdatePlayable, None)
+
+    val resultGameLabel = getPrivateField[Label]("resultGame")
+    resultGameLabel.getText shouldBe "You Win!"
+  }
+
+  "UpdateView" should "clear grids and initialize timer on Initialize" in runOnFXThread {
+    when(mockController.gameState).thenReturn(GameState.InGame)
+    gameView.updateView(Initialize, None)
+
+    val attemptGrid = getPrivateField[GridPane]("attemptGrid")
+    val hintGrid = getPrivateField[GridPane]("hintGrid")
+    val timeLabel = getPrivateField[Label]("timeLabel")
+
+    assert(attemptGrid.getChildren.size == 40)
+    assert(hintGrid.getChildren.size == 40)
+    timeLabel.getText shouldBe "Time: 00:00"
+  }
+
+  "UpdateView" should "display 'You Lose!' when game state is PlayerLose" in runOnFXThread {
+    when(mockController.gameState).thenReturn(GameState.PlayerLose)
+    gameView.updateView(Initialize, None)
+
+    getPrivateField[Label]("resultGame").getText shouldBe "You Lose!"
+  }
+
+  "UpdateView" should "update hint grid for UpdateHint type" in runOnFXThread {
+    when(mockController.gameState).thenReturn(GameState.InGame)
+    when(mockController.turn).thenReturn(0)
+    val testHints = Vector(HintRed, HintWhite, HintWhite, HintWhite, HintRed)
+
+    gameView.updateView(UpdateHint, Some(testHints))
+
+    val hintGrid = getPrivateField[GridPane]("hintGrid")
+    val hintLabels = hintGrid.getChildren.asScala
+      .collect { case label: Label => label.getText }
+
+    hintLabels should contain allOf ("Red", "White, White, White", "Red")
+  }
