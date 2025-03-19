a6df29fc320ee575ba8d839a302f76c2e7999a1b
Totti00 <gek5800@gmail.com>
2025-03-19 15:31:18 +0100

    added test for game mode

--- src/test/scala/mastermind/InitializeGridTest.scala
+++
@@ -1,5 +0,0 @@
-package mastermind
-
-import org.scalatest.flatspec.AnyFlatSpec
-
-class InitializeGridTest extends AnyFlatSpec
--- src/test/scala/mastermind/ManageGameTest.scala
+++
@@ -1,5 +0,0 @@
-package mastermind
-
-import org.scalatest.flatspec.AnyFlatSpec
-
-class ManageGameTest extends AnyFlatSpec
