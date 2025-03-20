55a09bba525055e579aa0a01d4f7fae5d83310ee
Totti00 <gek5800@gmail.com>
2025-03-20 14:31:49 +0100

    finished compareToEqual in prolog

--- src/main/scala/mastermind/TestProlog.scala
+++ src/main/scala/mastermind/TestProlog.scala
@@ -2,20 +2,30 @@ package mastermind
 
 import alice.tuprolog.Term
 import mastermind.Scala2P.createEngine
+import mastermind.model.entity.{HintStone, HintStones}
 
 object TestProlog extends App:
   val engine = createEngine("/prolog/code.pl")
 
-  //engine("permutation([1,2,3],L)") foreach (println(_))
-  // permutation ([1 ,2 ,3] ,[1 ,2 ,3]) ... permutation ([1 ,2 ,3] ,[3 ,2 ,1])
-
-  //val l = List("HintStones")
-  val l = List("HintStones")
-
+  var l = List("HintStones")
   for
     s <- engine(
         Term.createTerm("compareToEqual([green, green, blu, blu], [green, green, red, blu], HintStones).")
     )
     x = Scala2P.extractTermsToListOfStrings(s, l)
-    //hintStones = mkHintStonesVector(x)
-  yield println(x)
+    hintStones = mkHintStonesVector(x)
+  yield println(hintStones)
+
+  private def mkHintStonesVector(l: List[String]): HintStones = l match
+    case List(hintStones) => hintStones.split(",").map(_ => HintStone.HintRed).toVector
+    case _ => Vector.empty
+    
+    
+  l = List("HintStones")
+  for
+    s <- engine(
+      Term.createTerm("compareToPresent([green, green, blu, blu], [blu, blu, red, red], HintStones).")
+    )
+    x = Scala2P.extractTermsToListOfStrings(s, l)
+    hintStones = mkHintStonesVector(x)
+  yield println(hintStones)
