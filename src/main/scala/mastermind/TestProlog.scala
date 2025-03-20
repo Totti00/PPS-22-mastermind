87f93fdc1a1272a065db99e75f381ad1096424d9
Ramzi Gallala <ramzi.gallala@studio.unibo.it>
2025-03-20 14:29:27 +0100

    implemented code generator with ToProlog

--- src/main/scala/mastermind/TestProlog.scala
+++ src/main/scala/mastermind/TestProlog.scala
@@ -6,16 +6,16 @@ import mastermind.Scala2P.createEngine
 object TestProlog extends App:
   val engine = createEngine("/prolog/code.pl")
 
-  //engine("permutation([1,2,3],L)") foreach (println(_))
+  // engine("permutation([1,2,3],L)") foreach (println(_))
   // permutation ([1 ,2 ,3] ,[1 ,2 ,3]) ... permutation ([1 ,2 ,3] ,[3 ,2 ,1])
 
-  //val l = List("HintStones")
+  // val l = List("HintStones")
   val l = List("HintStones")
 
   for
     s <- engine(
-        Term.createTerm("compareToEqual([green, green, blu, blu], [green, green, red, blu], HintStones).")
+      Term.createTerm("compareToEqual([green, green, blu, blu], [green, green, red, blu], HintStones).")
     )
     x = Scala2P.extractTermsToListOfStrings(s, l)
-    //hintStones = mkHintStonesVector(x)
+  // hintStones = mkHintStonesVector(x)
   yield println(x)
